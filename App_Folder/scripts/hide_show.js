11cd2e122b6e6ee2c6d1d7d44da156241dc9e0d6
xuedesign <elmo@xuedesign.tw>
2013-11-07 21:57:54 +0800

    內頁模板建立 80% #20131107

--- App_Folder/scripts/hide_show.js
+++ App_Folder/scripts/hide_show.js
@@ -1,46 +1,35 @@
-// 顯示隱藏區塊
-function $(element){
-return element = document.getElementById(element);
+// JavaScript Document
+$(document).ready(function(){
+
+$("#leftFrame_display").click(function(){
+
+var fs = parent.document.getElementsByTagName("frameset")[1];
+
+if($("#left_staus").val() == "打開"){
+
+$("#left_staus").val("關閉");
+closeFrameset();
+
+}else if($("#left_staus").val() == "關閉"){
+
+$("#left_staus").val("打開");
+openFrameset();
 }
-function $D(){
-var d=$('class1content');
-var h=d.offsetHeight;
-var maxh=300;
-function dmove(){
-h+=50; //设置层展开的速度
-if(h>=maxh){
-d.style.height='';
-clearInterval(iIntervalId);
-}else{
-d.style.display='block';
-d.style.height=h+'px';
+
+
+});                           
+
+});
+
+
+function closeFrameset(){
+var fs = parent.document.getElementsByTagName("frameset")[1];
+fs.cols = "0,*";
+$("#leftFrame_display").text("展開選單");
 }
-}
-iIntervalId=setInterval(dmove,2);
-}
-function $D2(){
-var d=$('class1content');
-var h=d.offsetHeight;
-//var maxh=300;
-function dmove(){
-h-=50;//设置层收缩的速度
-if(h<=0){
-d.style.display='none';
-clearInterval(iIntervalId);
-}else{
-d.style.height=h+'px';
-}
-}
-iIntervalId=setInterval(dmove,2);
-}
-function $use(){
-var d=$('class1content');
-var sb=$('statebut');
-if(d.style.display=='none'){
-$D();
-sb.innerHTML='縮小搜尋列';
-}else{
-$D2();
-sb.innerHTML='展開搜尋列';
-}
-}
+
+function openFrameset(){
+var fs = parent.document.getElementsByTagName("frameset")[1];
+fs.cols = "202,*";
+$("#leftFrame_display").text("隱藏選單");
+} 
